97755266e01581c81b0bc913900b141f95dafb2f
Pazu Cheung <pforpazu@gmail.com>
2023-02-15 00:52:13 -0700

    carousel link redirect

--- frontend/src/components/ProductCarouselComponent.js
+++ frontend/src/components/ProductCarouselComponent.js
@@ -7,27 +7,26 @@ const ProductCarouselComponent = ({ bestSellers }) => {
     };
     return bestSellers.length > 0 ? (
         <Carousel>
-            {bestSellers &&
-                bestSellers.map((item, idx) => (
-                    <Carousel.Item key={idx}>
-                        <Carousel.Caption>
-                            <img
-                                crossOrigin="anonymous"
-                                className="d-block w-100"
-                                style={{ height: "300px", objectFit: "cover" }}
-                                src={item.images ? item.images[0].path : null}
-                                alt="First slide"
-                            />
-                            <LinkContainer
-                                style={cursorP}
-                                to={`/product-details/${item._id}`}
-                            >
-                                <h3>Bestseller in {item.category} Category</h3>
-                            </LinkContainer>
-                            <p>{item.description}</p>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                ))}
+            {bestSellers.map((item, idx) => (
+                <Carousel.Item key={idx}>
+                    <Carousel.Caption>
+                        <img
+                            crossOrigin="anonymous"
+                            className="d-block w-100"
+                            style={{ height: "300px", objectFit: "cover" }}
+                            src={item.images ? item.images[0].path : null}
+                            alt="First slide"
+                        />
+                        <LinkContainer
+                            style={cursorP}
+                            to={`/product-details/${item._id}`}
+                        >
+                            <h3>Bestseller in {item.category} Category</h3>
+                        </LinkContainer>
+                        <p>{item.description}</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            ))}
         </Carousel>
     ) : null;
 };
