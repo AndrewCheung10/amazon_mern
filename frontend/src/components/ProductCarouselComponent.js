e8a6a0a8730a6021e71b568ae8aca5bbcfa06cb3
Pazu Cheung <pforpazu@gmail.com>
2023-02-14 21:51:01 -0700

    change carousel click area

--- frontend/src/components/ProductCarouselComponent.js
+++ frontend/src/components/ProductCarouselComponent.js
@@ -11,22 +11,22 @@ const ProductCarouselComponent = ({ bestSellers }) => {
             {bestSellers &&
                 bestSellers.map((item, idx) => (
                     <Carousel.Item key={idx}>
-                        <img
-                            crossOrigin="anonymous"
-                            className="d-block w-100"
-                            style={{ height: "300px", objectFit: "cover" }}
-                            src={item.images ? item.images[0].path : null}
-                            alt="First slide"
-                        />
-                        <Carousel.Caption>
-                            <LinkContainer
-                                style={cursorP}
-                                to={`/product-details/${item._id}`}
-                            >
+                        <LinkContainer
+                            style={cursorP}
+                            to={`/product-details/${item._id}`}
+                        >
+                            <img
+                                crossOrigin="anonymous"
+                                className="d-block w-100"
+                                style={{ height: "300px", objectFit: "cover" }}
+                                src={item.images ? item.images[0].path : null}
+                                alt="First slide"
+                            />
+                            <Carousel.Caption>
                                 <h3>Bestseller in {item.category} Category</h3>
-                            </LinkContainer>
-                            <p>{item.description}</p>
-                        </Carousel.Caption>
+                                <p>{item.description}</p>
+                            </Carousel.Caption>
+                        </LinkContainer>
                     </Carousel.Item>
                 ))}
         </Carousel>
