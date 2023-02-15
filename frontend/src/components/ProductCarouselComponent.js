6c803eff42c92634de8ecefba80889208886ee62
Pazu Cheung <pforpazu@gmail.com>
2023-02-14 20:44:26 -0700

    check if bestSeller is null

--- frontend/src/components/ProductCarouselComponent.js
+++ frontend/src/components/ProductCarouselComponent.js
@@ -8,26 +8,27 @@ const ProductCarouselComponent = ({ bestSellers }) => {
     console.log(bestSellers);
     return bestSellers.length > 0 ? (
         <Carousel>
-            {bestSellers.map((item, idx) => (
-                <Carousel.Item key={idx}>
-                    <img
-                        crossOrigin="anonymous"
-                        className="d-block w-100"
-                        style={{ height: "300px", objectFit: "cover" }}
-                        src={item.images ? item.images[0].path : null}
-                        alt="First slide"
-                    />
-                    <Carousel.Caption>
-                        <LinkContainer
-                            style={cursorP}
-                            to={`/product-details/${item._id}`}
-                        >
-                            <h3>Bestseller in {item.category} Category</h3>
-                        </LinkContainer>
-                        <p>{item.description}</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-            ))}
+            {bestSellers &&
+                bestSellers.map((item, idx) => (
+                    <Carousel.Item key={idx}>
+                        <img
+                            crossOrigin="anonymous"
+                            className="d-block w-100"
+                            style={{ height: "300px", objectFit: "cover" }}
+                            src={item.images ? item.images[0].path : null}
+                            alt="First slide"
+                        />
+                        <Carousel.Caption>
+                            <LinkContainer
+                                style={cursorP}
+                                to={`/product-details/${item._id}`}
+                            >
+                                <h3>Bestseller in {item.category} Category</h3>
+                            </LinkContainer>
+                            <p>{item.description}</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                ))}
         </Carousel>
     ) : null;
 };
